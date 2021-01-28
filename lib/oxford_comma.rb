f1ac55dbe81140fdb7f994788814d1747673878a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-28 22:11:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array.join(",") && array.insert(2,"and")
+    array.join(0..1",") && array.insert(2,"and")
   end
 end
